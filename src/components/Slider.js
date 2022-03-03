903058bb8b882291dcc428aa297df44fc11def94
Matthieu Clay <matthieu.clay@outlook.fr>
2022-03-03 16:27:03 +0100

    :tada: initial commit

---
+++ src/components/Slider.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Slider.scss";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+function Slider() {
+  return <div>Slider</div>;
+}
+
+export default Slider;
